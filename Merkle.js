334c47240ea87f804f38ec28b851a392ac71e386
Parsh <parsh.cosmos11@gmail.com>
2020-10-29 19:54:05 +0530

    txn verification: merkle path

--- Merkle.js
+++ Merkle.js
@@ -49,4 +49,47 @@ class Merkle {
     const merkleRoot = this.merkleTree[0][0];
     return merkleRoot;
   }
+
+  hasTransaction(transactionId) {
+    // returns whether the txn is included or not in the cached merkle tree, in case of containment a merkle path is provided along w/ merkle root for self validation
+
+    if (!this.merkleTree.length)
+      throw new Error(
+        "Please generate a merkle tree prior to checking for containment"
+      );
+
+    const txHash = Merkle.doubleSHA256(transactionId);
+
+    // validate against the base merkle layer (first layer elimination for non-contained txn)
+    const baseMerkleLayer = this.merkleTree[this.merkleTree.length - 1];
+    if (!baseMerkleLayer.includes(txHash)) {
+      // returns false if txHash for the supplied txId is not present in the base layer
+      return { hasTransaction: false, merklePath: null };
+    } else {
+      // returns the merkle path w/ merkle root
+      const merklePath = [];
+      let merkleHash = txHash;
+      for (const merkleLayer of [...this.merkleTree].reverse()) {
+        if (merkleLayer.length < 2) break; // break away at merkle root
+
+        const merkleIndex = merkleLayer.indexOf(merkleHash);
+
+        if (merkleIndex % 2 == 0) {
+          const nextMerkleHash = merkleLayer[merkleIndex + 1];
+          merklePath.push({ hash: nextMerkleHash, offset: 1 }); // offset is used during self validation of tx-inclusion using the merkle path
+          merkleHash = Merkle.doubleSHA256(merkleHash + nextMerkleHash);
+        } else {
+          const prevMerkleHash = merkleLayer[merkleIndex - 1];
+          merklePath.push({ hash: prevMerkleHash, offset: -1 });
+          merkleHash = Merkle.doubleSHA256(prevMerkleHash + merkleHash);
+        }
+      }
+
+      return {
+        hasTransaction: true,
+        merklePath,
+        merkleRoot: this.merkleTree[0][0],
+      };
+    }
+  }
 }
